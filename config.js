ac4ecd6a709f99e07d6f45edff0f100115fb50df
pkiet8227-cmyk <pkiet8227@gmail.com>
2026-09-03 23:15:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.SUPABASE_URL =
-  "https://cvlkvyiqgnqvaueyjnbq.supabase.co/rest/v1/";
+  "https://cvlkvyiqgnqvaueyjnbq.supabase.co";
 
 window.SUPABASE_ANON_KEY =
   "sb_publishable_bBz3F7hS3FtgsDWHisqhmw_LL_h6XGr";
